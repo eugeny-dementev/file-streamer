0e0d0171ab68ba3f97b91f3dde5baca134ef72d5
Eugeny Dementev <eugeny.dementev@ya.ru>
2022-06-24 19:49:22 +0800

    aggregate values on the go

--- browser.js
+++ browser.js
@@ -1,25 +1,72 @@
-const utf8Decoder = new TextDecoder();
+class Aggregator {
+  constructor(splitter) {
+    this.splitter = splitter;
+    this.valuesSum = {};
+  }
+
+  parse(text) {
+    return text
+      .split('\r\n')
+      .map(line => {
+        const data = line.split(this.splitter);
+        const [id1, id2, id3, value1, value2] = data;
+        const dataChunk = {
+          id1, id2, id3,
+          value1: Number(value1),
+          value2: Number(value2),
+        }
+
+
+        return dataChunk;
+      });
+  }
+
+  aggregate(text) {
+    this
+      .parse(text)
+      .forEach(({id1, id2, id3, value1, value2}) => {
+        this.add('id1', id1, value1, value2);
+        this.add('id2', id2, value1, value2);
+        this.add('id3', id3, value1, value2);
+      });
+  }
+
+  add(key, id, value1, value2) {
+    if (this.valuesSum[key] && this.valuesSum[key][id]) {
+      this.valuesSum[key][id]['value1'] += value1;
+      this.valuesSum[key][id]['value2'] += value2;
+    } else {
+      this.valuesSum[key] = this.valuesSum[key] ? this.valuesSum[key] : {};
+      this.valuesSum[key][id] = {
+        value1: 0,
+        value2: 0,
+      }
+    }
+  }
+
+  getSum() {
+    return this.valuesSum;
+  }
+}
 
 fetch('http://localhost:1337')
-    .then(response => response.body.getReader())
-    .then((reader) => {
-        return new ReadableStream({
-            start(controller) {
-                return pump();
-                function pump() {
-                    return reader.read().then(({ done, value }) => {
-                        if (done) {
-                            controller.close();
-                            return;
-                        }
+  .then(response => response.body.getReader())
+  .then(async (reader) => {
+    const utf8Decoder = new TextDecoder();
 
-                        console.log('value:', utf8Decoder.decode(value));
+    const aggregator = new Aggregator(',');
 
-                        controller.enqueue(value);
-                        return pump();
-                    })
-                }
-            }
-        })
-    })
-    .catch(console.error);
+    while (true) {
+      const {done, value} = await reader.read();
+
+      if (done) break;
+
+      const text = utf8Decoder.decode(value);
+
+      aggregator.aggregate(text);
+    }
+
+    return aggregator.getSum();
+  })
+  .then((result) => console.log('result:', result))
+  .catch(console.error);
